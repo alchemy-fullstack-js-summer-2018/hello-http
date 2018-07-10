9bee72838fce6b1ec28f87a10852613e24f6b294
MariahAdams <mariah.adams23@yahoo.com>
2018-07-10 16:45:57 -0700

    added scaffolding to app and test file

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../lib/app');
+
+describe('', () => {
+
+    it('', () => {
+
+    });
+});

18aadba7ac720fa8a66e8e7e8d63c0a20fdbaa35
Mark Myers <markalope@gmail.com>
2018-07-10 16:58:14 -0700

    wrote first part of server test in app.js let us see if this runs

--- test/app.test.js
+++ test/app.test.js
@@ -5,3 +5,15 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 const app = require('../lib/app');
+
+describe('simple HTTP server', () => {
+
+    it('responds with Hello World on GET', () => {
+        return chai.request(app)
+            .get('/')
+            .then(res => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'Hello World');
+            });
+    });
+});

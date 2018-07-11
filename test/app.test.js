f0c8985faa550850c1a747754be03193354676fa
MariahAdams <mariah.adams23@yahoo.com>
2018-07-11 12:22:45 -0700

    refactored code to remove hardcoded test variables and be more dynamic

--- test/app.test.js
+++ test/app.test.js
@@ -9,33 +9,33 @@ describe('hello http server', () => {
 
     it('responds with happy birthday jane on GET', () => {
         return chai.request(app)
-            .get('/happy-birthday/name')
+            .get('/happy-birthday/Jane')
             .then(res => {
-                assert.equal(res.text, '<html><body><p>Happy Birthday <strong>Jane!</strong></p></body></html>');
+                assert.equal(res.text, '<html><body><p>Happy Birthday <strong>Jane!</strong> </p></body></html>');
             });
     });
 
     it('responds to a specific name', () => {
         return chai.request(app)
-            .get('/happy-birthday/name')
+            .get('/happy-birthday/Frank')
             .then(res => {
-                assert.equal(res.text, '<html><body><p>Happy Birthday <strong>Jane!</strong></p></body></html>');
+                assert.equal(res.text, '<html><body><p>Happy Birthday <strong>Frank!</strong> </p></body></html>');
             });
     });
 
     it('responds to a custom value on GET', () => {
         return chai.request(app)
-            .get('/happy-birthday/name?custom=You%20Rock')
+            .get('/happy-birthday/Jane?custom=You%20Rock')
             .then(res => {
                 assert.equal(res.text, '<html><body><p>Happy Birthday <strong>Jane!</strong> You Rock</p></body></html>');
             });
     });
 
-    it.only('responds with one random fact (of three) about http on GET', () => {
+    it('responds with one random fact (of three) about http on GET', () => {
         return chai.request(app)
             .get('/fact')
             .then(res => {
-                assert.ok(/http/.test(res.text));
+                assert.ok(/HTTP/.test(res.text));
             });
     });
 });

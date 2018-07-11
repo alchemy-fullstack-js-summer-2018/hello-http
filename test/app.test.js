27cbcc4e6ff98e3b3a2799e7907cef6901a1a9f0
Robyn Navarro <rnavarro182@gmail.com>
2018-07-11 09:48:00 -0700

    first get test passed

--- test/app.test.js
+++ test/app.test.js
@@ -2,17 +2,20 @@ const chai = require('chai');
 const { assert } = chai;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-
 const app = require('../lib/app');
 
 describe('Simple http server', () => {
 
-    it('responds with happy birthday greeting', () => {
+    it('responds with happy birthday greeting on GET', () => {
         return chai.request(app)
-            .get('/')
+            .get('/happy-birthday')
             .then(res => {
                 assert.equal(res.status, 200);
                 assert.equal(res.text, 'Happy birthday');
             });
     });
+
+    it('responds happy birth day with name', () => {
+        
+    })
 });

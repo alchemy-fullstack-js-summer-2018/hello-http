6f71c9bcd0b0284bc67cd1190f4732b87388b97b
Robyn Navarro <rnavarro182@gmail.com>
2018-07-10 17:41:44 -0700

    initial commit

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../lib/app');
+
+describe('Simple http server', () => {
+
+    it('responds with happy birthday greeting', () => {
+        return chai.request(app)
+            .get('/')
+            .then(res => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'Happy birthday');
+            });
+    });
+});

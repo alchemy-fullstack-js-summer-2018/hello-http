188322c8a68078f67361159aed7f880b5abaddc3
Easton Gorishek <ieaston26@gmail.com>
2018-07-11 11:55:53 -0700

    fixed chai error

--- test/app.test.js
+++ test/app.test.js
@@ -8,11 +8,11 @@ const app = require('../lib/app');
 
 describe('simple http server', () => {
 
-    it('responds with hello Easton on GET', () => {
+    it('responds with hello Ninja on GET', () => {
         return chai.request(app)
             .get('/')
             .then(res => {
-                assert.equal(res.text, 'Hello Easton!');
+                assert.equal(res.text, 'Hello Ninja!');
             });
     });
 

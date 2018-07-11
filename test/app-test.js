ea44e78aff892de690e5a2f8f2d37a5e1f425ad1
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-10 18:20:05 -0700

    Test Response Hello World on GET Passing

---
+++ test/app-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../lib/app');
+
+describe('simple htt server', () => {
+
+    it('responds with hello world on GET', () => {
+        return chai.request(app)
+            .get('/')
+            .then(res => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'hello world');
+            });
+    });
+});

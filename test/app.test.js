f6ae39122ef648b32ee5eadec0dd061776339435
Robyn Navarro <rnavarro182@gmail.com>
2018-07-11 11:35:21 -0700

    all happy birthday tests passing

--- test/app.test.js
+++ test/app.test.js
@@ -11,11 +11,41 @@ describe('Simple http server', () => {
             .get('/happy-birthday')
             .then(res => {
                 assert.equal(res.status, 200);
-                assert.equal(res.text, 'Happy birthday');
+                assert.equal(res.text, 'Happy birthday, Stranger!');
             });
     });
 
-    it('responds happy birth day with name', () => {
-        
-    })
+    it('responds happy birthday with name', () => {
+        return chai.request(app)
+            .get('/happy-birthday/Longa')
+            .then(res => {
+                assert.equal(res.text, 'Happy birthday, Longa!');
+            });
+    });
+
+    it('responds with custom message', () => {
+        return chai.request(app)
+            .get('/happy-birthday/Longa?custom=%20Another%20year%20of%20mangling%20lizards...')
+            .then(res => {
+                assert.equal(res.text, 'Happy birthday, Longa! Another year of mangling lizards...');
+            });
+    });
+    
+    it('responds with 404', () => {
+        return chai.request(app)
+            .get('/unhappy-birthday')
+            .then(res => {
+                assert.equal(res.status, 404);
+                assert.equal(res.text, 'Not found');
+            });
+    });
+
+    it('responds with a random http fact', () => {
+        return chai.request(app)
+            .get('/facts')
+            .then(res => {
+                assert.include(res.text, 'http');
+            });
+    });
+
 });

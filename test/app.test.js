ca1f8c4efd10b84f6bcd110c2d3dd3d3a9d86a1d
Rehmsy <sarahrehmer@gmail.com>
2018-07-10 17:41:27 -0700

    first test failing

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const { assert } = chai;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const app = require('../lib/app');
+
+describe('not-simple-to-me http server', () => {
+
+    it('responds with waz up world on GET', () => {
+        return chai.request(app)
+            .get('/')
+            .then(res => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, 'waz up world');
+            });
+    });
+});

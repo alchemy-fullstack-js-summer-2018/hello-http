701d1fca4f173a6bbfe46cefef78e5c31e205071
rbtprograms <rbt61690@gmail.com>
2018-07-10 17:53:44 -0700

    second test passing and is bringing back a random fact

--- test/app.test.js
+++ test/app.test.js
@@ -6,15 +6,23 @@ chai.use(chaiHttp);
 const app = require('../lib/app');
 
 describe('simple http server', () => {
+    const birthdayRes = '<html><body><p>Happy Birthday <strong>Stranger!</strong></p></body></html>';
 
     it('responds with hello world on GET', () => {
         return chai.request(app)
             .get('/happy-birthday')
             .then(res => {
                 assert.equal(res.status, 200);
-                assert.equal(res.text, 
-                    '<html><body><p>Happy Birthday <strong>Stranger!</strong> You Rock</p></body></html>'
-                );
+                assert.equal(res.text, birthdayRes);
+            });
+    });
+
+    it('responds with a random fact when you ask for one', () => {
+        return chai.request(app)
+            .get('/facts')
+            .then(res => {
+                assert.equal(res.status, 200);
+                console.log(res.text);
             });
     });
 
